36da6fb29f7545db51486b8648ce649719a48018
Grigoriy Kislyi <RyhorKisly@gmail.com>
2023-10-25 17:17:46 +0300

    add: aspectJ

---
+++ src/main/java/io/ylab/walletservice/aop/aspects/AuditableAspect.java
@@ -0,0 +1,42 @@
+package io.ylab.walletservice.aop.aspects;
+
+import io.ylab.walletservice.core.dto.AuditDTO;
+import io.ylab.walletservice.service.api.IAuditService;
+import io.ylab.walletservice.service.factory.AuditServiceFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Class for creating audit
+ */
+@Aspect
+public class AuditableAspect {
+
+    /**
+     * Define a field with a type {@link IAuditService} for further aggregation
+     */
+    private final IAuditService auditService = AuditServiceFactory.getInstance();
+
+
+    /**
+     * pointcut which say that methods or classes marked with annotation {@link io.ylab.walletservice.aop.annotations.Auditable}
+     * have to be executed
+     */
+    @Pointcut("within(@io.ylab.walletservice.aop.annotations.Auditable *) && execution(* *(..))")
+    public void annotatedByAuditable() {
+    }
+
+    /**
+     * Aspect for creating audit
+     */
+    @Around("annotatedByAuditable()")
+    public Object audit(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result = proceedingJoinPoint.proceed();
+        AuditDTO dto = new AuditDTO();
+        dto.setText("Action was performed on the entity by method: " + proceedingJoinPoint.getSignature().toShortString());
+        auditService.create(dto);
+        return result;
+    }
+}
---
+++ src/main/java/io/ylab/walletservice/aop/aspects/LoggableAspect.java
@@ -0,0 +1,36 @@
+package io.ylab.walletservice.aop.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Class for creating logs
+ */
+@Aspect
+public class LoggableAspect {
+
+    /**
+     * pointcut which say that methods or classes marked with annotation {@link io.ylab.walletservice.aop.annotations.Loggable}
+     * have to be executed
+     */
+    @Pointcut("within(@io.ylab.walletservice.aop.annotations.Loggable *) && execution(* *(..))")
+    public void annotatedByLoggable() {
+    }
+
+    /**
+     * Aspect for creating logs. We can see when method called, executed and time of execution
+     */
+    @Around("annotatedByLoggable()")
+    public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("Calling method " + proceedingJoinPoint.getSignature().toShortString());
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution of method " + proceedingJoinPoint.getSignature().toShortString() +
+                " finished. Execution time is " + (endTime - startTime) + " ms");
+        return result;
+    }
+
+}
