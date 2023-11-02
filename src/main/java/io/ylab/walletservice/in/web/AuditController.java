aa5756d3d21e7bcb7931cc33921da917867e1b91
Grigoriy Kislyi <RyhorKisly@gmail.com>
2023-11-02 18:25:02 +0300

    add: controllers

---
+++ src/main/java/io/ylab/walletservice/in/web/AuditController.java
@@ -0,0 +1,47 @@
+package io.ylab.walletservice.in.web;
+
+import io.ylab.walletservice.aop.annotations.Loggable;
+import io.ylab.walletservice.config.properties.JWTProperties;
+import io.ylab.walletservice.core.dto.AuditDTO;
+import io.ylab.walletservice.core.mappers.AuditMapper;
+import io.ylab.walletservice.dao.entity.AuditEntity;
+import io.ylab.walletservice.service.api.IAuditService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Controller for operations with audits
+ */
+@RestController
+@AllArgsConstructor
+@Loggable
+public class AuditController {
+
+    /**
+     * Define a field with a type {@link IAuditService} for further aggregation
+     */
+    private final IAuditService auditService;
+
+    /**
+     * Define a field with a type {@link AuditMapper} for further interaction
+     */
+    private final AuditMapper auditMapper;
+
+    /**
+     * Called by the server to allow a controller to handle a GET request. Find all audits.
+     * @return status and {@link List<AuditDTO>}
+     */
+    @GetMapping("/users/audit")
+    public ResponseEntity<List<AuditDTO>> findAll() {
+        List<AuditEntity> auditsEntity = auditService.getAll();
+        List<AuditDTO> auditsDTO = auditMapper.toDTOSet(auditsEntity);
+        return new ResponseEntity<>(auditsDTO, HttpStatus.OK);
+
+    }
+
+}
---
+++ src/main/java/io/ylab/walletservice/in/web/AuthenticationController.java
@@ -0,0 +1,112 @@
+package io.ylab.walletservice.in.web;
+
+import io.ylab.walletservice.aop.annotations.Loggable;
+import io.ylab.walletservice.core.dto.UserAuthenticationDTO;
+import io.ylab.walletservice.core.dto.UserDTO;
+import io.ylab.walletservice.core.exceptions.BlankBodyFieldsException;
+import io.ylab.walletservice.core.exceptions.InvalidLoginException;
+import io.ylab.walletservice.core.exceptions.SizeLengthException;
+import io.ylab.walletservice.core.mappers.UserMapper;
+import io.ylab.walletservice.dao.entity.UserEntity;
+import io.ylab.walletservice.service.api.IUserAuthenticationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Controller for authentication operations with user
+ */
+@RestController
+@AllArgsConstructor
+@Loggable
+public class AuthenticationController {
+
+    /**
+     * Pattern for validating login like email xxx@xxx.xxx
+     */
+    private static final String EMAIL_PATTERN = "[^@]+@[^@]+\\.[^@.]+";
+
+    /**
+     * Message if entered login has wrong pattern.
+     */
+    private static final String WRONG_LOGIN_PATTERN = "Login should be like email: xxx@xxx.xxx";
+
+    /**
+     * Message if login null or empty
+     */
+    private static final String LOGIN_MANDATORY = "Login is mandatory!";
+
+    /**
+     * Message if password null or empty
+     */
+    private static final String PASSWORD_MANDATORY = "Passowrd is mandatory!";
+
+    /**
+     * Message if password length is wrong
+     */
+    private static final String WRONG_PASSWORD_LENGTH = "Password should be more than 4 or less than 20 symbols";
+
+    /**
+     * Define a field with a type {@link IUserAuthenticationService} for further aggregation
+     */
+    private final IUserAuthenticationService authenticationService;
+
+    /**
+     * Define a field with a type {@link UserMapper} for further aggregation
+     */
+    private final UserMapper userMapper;
+
+    /** Called by the server to allow a controller to handle a POST request to register user.
+     * @return status and {@link UserDTO}
+     */
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(
+            @RequestBody UserAuthenticationDTO dto
+    ) {
+        validate(dto);
+            UserEntity userEntity = authenticationService.register(dto);
+            UserDTO userDTO = userMapper.toDTO(userEntity);
+
+            return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    }
+
+    /** Called by the server to allow a controller to handle a POST request to authorize user.
+     * @return status and {@link UserDTO}
+     */
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> authorize(
+            @RequestBody UserAuthenticationDTO dto
+    ) {
+        validate(dto);
+        return ResponseEntity.ok()
+                .header("Authorization", authenticationService.authorize(dto)).build();
+    }
+
+    /**
+     * Validate {@link UserAuthenticationDTO} obtained by parsing the json
+     * @param dto for validating
+     * @throws BlankBodyFieldsException if dto's field empty or null
+     * @throws SizeLengthException if dto's password has wrong length
+     * @throws InvalidLoginException if dto's login has structure different from xxx@xxx.xxx
+     */
+    private void validate(UserAuthenticationDTO dto) {
+        if(dto.getLogin() == null || dto.getLogin().isEmpty()) {
+            throw new BlankBodyFieldsException(LOGIN_MANDATORY);
+        } else if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+            throw new BlankBodyFieldsException(PASSWORD_MANDATORY);
+        } else if (dto.getPassword().length() <= 4 || dto.getPassword().length() >= 20) {
+            throw new SizeLengthException(WRONG_PASSWORD_LENGTH);
+        }
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(dto.getLogin());
+        if(!matcher.matches()) {
+            throw new InvalidLoginException(WRONG_LOGIN_PATTERN);
+        }
+    }
+}
