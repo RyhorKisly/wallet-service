bb4bda177e91e15f86f0849a8a7da0c9495f7868
Grigoriy Kislyi <RyhorKisly@gmail.com>
2023-11-02 18:28:41 +0300

    refactor: liquibase

--- src/test/java/io/ylab/walletsevice/dao/api/IConnectionWrapper.java
+++ src/test/java/io/ylab/walletsevice/dao/api/IConnectionWrapper.java
@@ -1,12 +1,15 @@
-package io.ylab.walletservice.dao.ds.api;
+package io.ylab.walletsevice.dao.api;
 
+import org.springframework.jdbc.datasource.embedded.DataSourceFactory;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
  * Interface with one method for getting connection (session) with a specific database
  */
-public interface IConnectionWrapper extends AutoCloseable{
+public interface IConnectionWrapper extends DataSource {
 
     /**
      * Method for getting connection (session) with a specific database
