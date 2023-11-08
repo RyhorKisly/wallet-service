a800ea3520a27e5c9889d62be6d2d57911787f28
Grigoriy Kislyi <RyhorKisly@gmail.com>
2023-11-08 13:09:30 +0300

    add: some configurations

---
+++ wallet-service/src/main/java/io/ylab/walletservice/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package io.ylab.walletservice.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
